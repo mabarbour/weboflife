8ecbbb9c5cdbc729faf8f7e0df58faabe83d7b24
mabarbour <matthew.a.barbour@gmail.com>
2019-03-25 10:28:37 +0100

    update description and get_network_info

--- R/get_network_info.R
+++ R/get_network_info.R
@@ -1,45 +1,47 @@
 #' Get network info from Web of Life
 #'
-#' @param type interaction type
-#' @param names would you like to include the names of species?
-#' @return list of networks
+#' @param interaction_type "pollination", "seed disperser", "plant-ant", "plant-herbivore", "anemone-fish", "host-parasite", "food web"
+#' @param data_type "binary", "weighted", or "all
+#' @return data frame with metadata for each network.
 #' @seealso
 #' @export
 #' @examples
 
-get_network_info <- function(type){
+get_network_info <- function(interaction_type, data_type){
 
   # create an empty list to store the networks
   network_list <- list()
 
   # nested ifelse statement to turn interaction types into codes for Web of Life
-  type_id <- ifelse(type == "plant-ant", "3",
-                    ifelse(type == "plant-pollinator", "5",
-                           ifelse(type == "plant-seed disperser", "6",
-                                  ifelse(type == "anemone-fish", "11",
-                                         ifelse(type == "host-parasite", "8",
-                                                ifelse(type == "plant-herbivore", "10",
-                                                       ifelse(type == "food web", "7", NA)))))))
+  type_id <- ifelse(interaction_type == "plant-ant", "3",
+                    ifelse(interaction_type == "pollination", "5",
+                           ifelse(interaction_type == "seed disperser", "6",
+                                  ifelse(interaction_type == "anemone-fish", "11",
+                                         ifelse(interaction_type == "host-parasite", "8",
+                                                ifelse(interaction_type == "plant-herbivore", "10",
+                                                       ifelse(interaction_type == "food web", "7", NA)))))))
 
-  # create a file (json_networks) with the names of the networks we would like to download
+  data_ID <- ifelse(data_type == "binary", "0",
+                    ifelse(data_type == "weighted", "1",
+                           ifelse(data_type == "all", "all", NA)))
+
+  # create a json file with metadata for each network
   json_file <- paste("http://www.web-of-life.es/map_networkdetails_type.php?networkType=",
-                     type_id,
-                     #"&data=All",
-                     sep = "")
+                     type_id, "&data=", data_ID, sep = "")
+
+  # turn json file into a data frame and return most relevant information
   json_network_info <- jsonlite::fromJSON(paste(readLines(json_file), collapse = "")) %>%
-    select(networkAuthor, networkQ, locationName, locationLatitude, locationLongitude) %>%
-    mutate(networkQ = ifelse(networkQ == "\001", "weighted","binary"),
-           locationLatitude = as.numeric(locationLatitude),
-           locationLongitude = as.numeric(locationLongitude))
+    transmute(name = networkName,
+              interaction_type = networkTypeName,
+              author = networkAuthor,
+              location = locationName,
+              latitude = as.numeric(locationLatitude),
+              longitude = as.numeric(locationLongitude),
+              data_type = ifelse(networkQ == "\001", "weighted","binary"),
+              S = as.numeric(countSpecies),
+              L = as.numeric(countConnections),
+              C = as.numeric(connectance))
 
-  #json_network_info_list <- lapply(json_network_info, FUN = function(x) as.data.frame(t(x))) #list()
-  #json_network_info_df <- plyr::ldply(json_network_info_list) %>%
-  #  select(networkAuthor, networkQ, locationName, locationLatitude, locationLongitude) %>%
-  #  mutate(networkQ = ifelse(networkQ == "\001", "Q","B"))
-  #for(i in 1:length(json_network_info)){
-  #  json_network_info_list <- t(json_network_info[[i]])
-  #}
-
-  return(json_network_info)#df)
+  return(json_network_info)
 }
 
